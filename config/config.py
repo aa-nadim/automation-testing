435bb5803c9ad029271428f1ad97c48e97cdc6c7
aa-nadim <nadim.ice.nstu@gmail.com>
2024-12-11 10:47:21 +0600

    Update URL and Run in headless mode

--- config/config.py
+++ config/config.py
@@ -1,6 +1,6 @@
 # config/config.py
 
-PROPERTY_URL = "https://www.alojamiento.io/property/charming-lavapi%c3%a9s-town-cozy-apartment-for-4-people-ac/HA-61511638942"
+PROPERTY_URL = "https://www.alojamiento.io/property/bonito-piso-luminoso-en-urbanizaci%c3%b3n-de-lujo/BC-2819656"
 LOG_FILE_PATH = "logs/test_logs.log"
 SCREENSHOT_DIR = "screenshots/"
 REPORT_FILE_PATH = "reports/test_result.xlsx"
